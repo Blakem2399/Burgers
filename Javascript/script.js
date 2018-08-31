239810bf8589ffdddc61d851b8484c0eec3421b2
Blake M <Blake.manookin@gmail.com>
2018-08-31 10:29:36 -0600

    fixed how colored text reacts to being put in a div added flexboxes to make site more mobile friendly broke burgertext. added hide feature to FAQ

---
+++ Javascript/script.js
@@ -0,0 +1,18 @@
+function reveal() {
+    var x = document.getElementById("expensive");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+function reveal2() {
+    var y = document.getElementById("beef");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+}
